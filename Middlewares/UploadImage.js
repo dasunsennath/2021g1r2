ecd706f42226c8e05854d07c8b71de4da77a8ab3
44DH1L <you@example.com>
2021-10-22 18:11:18 +0530

    Change configs

--- Middlewares/UploadImage.js
+++ Middlewares/UploadImage.js
@@ -1,27 +1,24 @@
-const multer  = require('multer');
-const cloudinary =require('cloudinary').v2;
-const {CloudinaryStorage} = require('multer-storage-cloudinary');
-
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key:process.env.CLOUDINARY_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:
-    {
-      folder: 'Quizzee',
-      allowedFormats:['jpg', 'png','jpeg','gif'],
-      filename: function (req, file) {
-        return file.originalname; 
-      }
+  cloudinary: cloudinary,
+  params: {
+    folder: "Quizzee",
+    allowedFormats: ["jpg", "png", "jpeg", "gif"],
+    filename: function (req, file) {
+      return file.originalname;
+    },
+  },
+});
 
-    }
-  });
-
-const Upload = multer({storage:storage});
+const Upload = multer({ storage: storage });
 
 module.exports = Upload;
