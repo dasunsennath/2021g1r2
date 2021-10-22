e764c447d3988015129dfc046fe40e1c9aacbbb8
44DH1L <you@example.com>
2021-10-22 18:15:29 +0530

    add Route for Course & Quiz

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-require('dotenv').config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var passport = require("passport");
+require("dotenv").config();
 
-
-
-var Profile = require('./routes/Profile')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var UserAdminRouter = require('./routes/users.admin');
+var Profile = require("./routes/Profile");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var UserAdminRouter = require("./routes/users.admin");
+var CourseRouter = require("./routes/Course");
+var QuizRouter = require("./routes/Quiz");
 
 var app = express();
 
-
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(passport.initialize());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/profile',Profile);
-app.use('/admin',UserAdminRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/profile", Profile);
+app.use("/admin", UserAdminRouter);
+app.use("/course", CourseRouter);
+app.use("/quiz", QuizRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
